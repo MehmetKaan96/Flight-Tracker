652bf90991b70a565d20475091b431a86e256510
Mehmet Kaan <mehmetkaan1996@gmail.com>
2023-11-19 14:08:45 +0300

    Added gradient background and created custom page

---
+++ FlightTracker/FlightTracker/View/CustomViews/CustomPage.swift
@@ -0,0 +1,20 @@
+//
+//  CustomPage.swift
+//  FlightTracker
+//
+//  Created by Mehmet Kaan on 19.11.2023.
+//
+
+import UIKit
+
+class CustomPage: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
